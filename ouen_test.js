e71a706d547296ef7d3bb5de43927021c7c8e5cc
Masayukiii <szkmsyk041792@gmail.com>
2017-07-06 16:42:34 +0900

    ページ移動なくす

--- ouen_test.js
+++ ouen_test.js
@@ -11,50 +11,28 @@ if (location.pathname.match(/^\/companies\/bizcast\/projects?$/)) {
       url: 'https://raw.githubusercontent.com/sqrtxx/benritools/master/ouen.txt',
       type: 'GET',
       success: function(data){
-        var text = data.replace(/\n/g, ',').replace(/[^0-9,]/g, '').toString();
-        var project_ids = text.substr( 0, text.length-1 ).split(',');
-        var first_project_id = project_ids[0];
-        window.location = "https://www.wantedly.com/projects/" + first_project_id;
+        var urls    = data.replace(/\n/g, ',').trim(',').split(',');
+        var url_idx = urls.indexOf(location.href);
+        for( var i=0; i < urls.length; i++  ) {
+          var project_id = urls[i].match(/\d+/)
+          var json = {"project_support":{"message":"","project_id": project_id,"post_to_fb_wall":false,"post_to_twitter":false,"post_to_linkedin":false}};
+          $.ajax({
+            type:"post",
+            url:project_id[i] + "/supports",
+            data:JSON.stringify(json),
+            contentType: 'application/json',
+            dataType: "json"
+          });
+          console.log("応援しています。。。")
+          if (i == urls.length - 1) {
+            alert('応援完了です！\n本日もご協力ありがとうございました！')
+            break;
+          } 
+        }
       }
     })
   }
-} else {
-  $.ajax({
-    url: 'https://raw.githubusercontent.com/sqrtxx/benritools/master/ouen.txt',
-    type: 'GET',
-    success: function(data){
-      var urls     = data.replace(/\n/g, ',').trim(',').split(',');
-      
-      var url_idx = urls.indexOf(location.href);
-      
-      if (url_idx != -1 && $(".project-support-link").length) {
-        GoForIt();
-        if (urls.length - 1 != url_idx) {
-          var next_url = urls[url_idx + 1]
-          console.log(next_url)
-          window.location = next_url;
-        } else {
-          alert('応援完了です！\n本日もご協力ありがとうございました！')
-        }
-      }
-    }
-  })
-}
-
-
-function GoForIt() {
-  $('.wt-button.blue.noborder.project-support-link.ng-isolate-scope').each(function(){
-    var project_id = $(this).data('project-id');
-    var json = {"project_support":{"message":"","project_id": project_id,"post_to_fb_wall":false,"post_to_twitter":false,"post_to_linkedin":false}};
-    $.ajax({
-        type:"post",
-        url:"/projects/" + project_id + "/supports",
-        data:JSON.stringify(json),
-        contentType: 'application/json',
-        dataType: "json"
-    });
-  });
-};
+} 
 
 function AllGoForIt() {
   $('.projects-index-single').each(function(){
