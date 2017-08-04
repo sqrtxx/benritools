68baecbecc61ff7d0aceeca17d299120313bea68
Hideo Takeuchi <h.takeuchi@bizcast.bz>
2017-08-04 19:05:27 +0900

    fix

--- ouen_test.js
+++ ouen_test.js
@@ -22,6 +22,7 @@ if (location.pathname.match(/^\/companies\/buysell-technologies\/projects?$/)) {
       }
     })
   }
+  alert('応援完了です！\n本日もご協力ありがとうございました！')
 }
 
 // 応援関数
@@ -59,4 +60,3 @@ function PostGoForIt(project_id) {
   })
 }
 
-alert('応援完了です！\n本日もご協力ありがとうございました！')
