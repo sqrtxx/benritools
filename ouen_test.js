d0615d1086787355e3fd3bd504b1325d27ccaebd
takemokun <takemo.takemokun@gmail.com>
2017-09-06 18:05:28 +0900

    応援リストの更新 (#39)
    * chnege ouen list
    
    * add finish message
    
    * fix
    
    * add finish message
    
    * respond to 20170615
    
    * change list 2017/06/21
    
    * 応援合戦の対象記事編集 (#13)
    
    * fix
    
    * change ouen target
    
    * 応援test作成 (#14)
    
    * test用コード作成
    
    * fix
    
    * fix (#16)
    
    * 一旦条件削除
    
    * reloadするように
    
    * reload重複しないように
    
    * fix
    
    * 応援済みなら終了
    
    * d
    
    * fix
    
    * Update ouen_test.js
    
    * test
    
    * hoge
    
    * fix
    
    * Update ouen_test.js
    
    * リファクタリング
    
    * 募集終了しているcandle削除
    
    * bizcast削除
    
    * 応援時間を１０秒に
    
    * chenge ouen list (#17)
    
    * ページ移動なくす
    
    * 修正
    
    * fix
    
    * 応援リスト更新 (#18)
    
    * chenge ouen list
    
    * change ouen list
    
    * 一旦削除
    
    * setTimeoutの追加 (#19)
    
    * chenge ouen list
    
    * change ouen list
    
    * add settimeout
    
    * リクエストの時間の感覚を空ける
    
    * リファクタリング
    
    * 一旦戻す
    
    * 遅延評価に対応する
    
    * fix
    
    * fix
    
    * ボタン判断ではなく、対象URLに変更
    
    * targeturlは保持させる
    
    * goforitはもとに戻す
    
    settimeoutは初回は時間を待たずに行うから既存のgoforitに切り替え
    
    * 応援速度遅めに
    
    * update ouen txt (#21)
    
    * 終わりを告げる
    
    * 終わりを告げる
    
    * 終わりを告げる
    
    * 応援リストの更新 (#22)
    
    * update ouen txt
    
    * chenge ouen target
    
    * 応援中はbitstar色に
    
    * 応援ページのみ
    
    * fix
    
    * fixfix
    
    * fixfixfix
    
    * メッセージ削除
    
    * 応援合戦リストの更新20170726 (#23)
    
    * update ouen txt
    
    * chenge ouen target
    
    * change ouen list
    
    * ロジックをもとに戻す(PV対策)
    
    * 応援中は背景色を変更
    
    * change ouen list (#24)
    
    * 応援合戦リスト更新20170802 (#25)
    
    * change ouen list
    
    * change ouen list
    
    * test (#26)
    
    * alretの修正 (#27)
    
    * test
    
    * fix
    
    * 応援リストの表記変更 (#28)
    
    * test
    
    * fix
    
    * テストの修正 (#29)
    
    応援ロジックの修正
    
    * add console log (#30)
    
    * change ouenlist (#31)
    
    * 応援合戦リストの更新 (#32)
    
    * change ouenlist
    
    * change ouen list
    
    * change ouen list 20170817
    
    * 応援合戦リストの更新 20170818 (#33)
    
    * change ouenlist
    
    * change ouen list
    
    * change ouen list 20170817
    
    * change ouen list
    
    * 応援リスト更新 (#35)
    
    * change ouenlist
    
    * change ouen list
    
    * change ouen list 20170817
    
    * change ouen list
    
    * change ouen list
    
    * 応援リスト更新 (#36)
    
    * change ouenlist
    
    * change ouen list
    
    * change ouen list 20170817
    
    * change ouen list
    
    * change ouen list
    
    * change ouen list
    
    * 応援合戦リスト更新 (#37)
    
    * change ouenlist
    
    * change ouen list
    
    * change ouen list 20170817
    
    * change ouen list
    
    * change ouen list
    
    * change ouen list
    
    * change daily ouen list
    
    * change ouen list (#38)
    
    * change ouen list

--- ouen_test.js
+++ ouen_test.js
@@ -18,10 +18,7 @@ if (location.pathname.match(/^\/companies\/buysell-technologies\/projects?$/)) {
         var project_ids = data.split(/\n/g);
         project_ids.pop();
         setTimeout(function(){
-          project_ids.forEach(function(project_id) {
-            console.log("post to " + project_id);
-            PostGoForIt(project_id);
-          })
+          project_ids.forEach(function(project_id) { PostGoForIt(project_id); })
         }, 2000)
         alert('応援完了です！\n本日もご協力ありがとうございました！')
       }
@@ -65,3 +62,4 @@ function PostGoForIt(project_id) {
     headers:     { 'X-Alt-Referer': referer }
   })
 }
+
